05710cccc41f85c765050c2361bd28e19dbbcf04
LinCanhui <lingyun2311@gmail.com>
2025-03-24 15:47:30 +0800

    FreeMarker 模板引擎整合使用教程

---
+++ src/main/java/com/mlinyun/springboot3demo/controller/FreeMarkerController.java
@@ -0,0 +1,18 @@
+package com.mlinyun.springboot3demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FreeMarkerController {
+
+    @GetMapping("/freemarker")
+    public ModelAndView freeMarker(@RequestParam(value = "info", required = false, defaultValue = "FreeMarker") String info) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("info", info);
+        modelAndView.setViewName("freemarker");
+        return modelAndView;
+    }
+}
